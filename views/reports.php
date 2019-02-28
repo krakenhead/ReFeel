01c5d7fe401cdc0109022545111974cdb2fd33f5
krakenhead <melandrewgetutua@gmail.com>
2019-03-01 03:53:12 +0800

    reports migrated

--- views/reports.php
+++ views/reports.php
@@ -21,24 +21,80 @@ include "../controller/fetchEmpAcc.php";
     <?php 
     include "components/sidebar.php";
     ?>
-    <div class="mainpanel">
+    <main class="mainpanel">
       <?php 
       include "components/header.php";
+      $datenow = date('Y-m-d');
       ?>
-      <div class="content">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum maxime similique mollitia vitae eaque corrupti inventore odit nobis aperiam quo labore ad perferendis, dolorum, voluptatem nesciunt sit vero repellat molestias.
+      <div class="page-title">
+        <h3>Reports</h3>
       </div>
-    </div>
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12 col-lg-12 p-0">
+              <div class="content-container">
+                <div class="row">
+                  <div class="form-group col-md-12">
+                    <h5 class="nunito">Select timeline</h5>
+                    <form id = "daterange" name = "daterange">
+                      <label>From:</label>
+                      <input type = "date" name="from_range" id = "from_range" class = "form-control-inline" value="<?php echo $datenow ?>">
+                      <label class="ml-3">To:</label>
+                      <input type="date" name="to_range" id="to_range" class="form-control-inline" value="<?php echo $datenow ?>">
+                      <button type="submit" class ="btn form-control-inline">Go</button>
+                      <button type="button" class="btn btn-outline-danger float-right clearfix mb-5" id="btn_genpdf" ><i class="fas fa-print"></i> Print Reports</button>
+                    </form>
+                  </div>
+                  <div id="rep" class="col-md-12 col-lg-12 p-0">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   </div>
-  <?php 
-  include "components/core-script.php";
-  ?>
-  <script src="../public/js/feather.min.js"></script>
+  <script src="../public/rep/jquery-3.3.1.min.js"></script>
+  <script src="../public/rep/popper.min.js"></script>
+  <script src="../public/rep/bootstrap.min.js"></script>
   <script>
-    feather.replace();
     $('#reports').addClass('active');
-    $(document).ready(function (){
-      $('.loader').hide();
+    $('.loader').hide();
+
+    var formdata = $("form[name='daterange']").serialize();
+    console.log(formdata);
+    $.ajax({
+      url: "../controller/reports/fetchReports.php",
+      type: "POST",
+      data:{formdata:formdata},
+      success:function(data){
+        // console.log(data);
+        $("#rep").html(data);
+        // document.getElementById('reports').innerHTML = data;
+      }
+    });
+
+    $(document).on("submit","form[name='daterange']",function(e){
+      e.preventDefault();
+      var formdata = $("form[name='daterange']").serialize();
+      $.ajax({
+        url: "../controller/reports/fetchReports.php",
+        type: "POST",
+        data:{formdata:formdata},
+        success:function(data){
+          // console.log(data);
+          // document.getElementById('reports').innerHTML = data;
+          $("#rep").html(data);
+        }
+      });
+    });
+
+    $(document).on("click","#btn_genpdf",function(e){
+      var from_range = $("#from_range").val();
+      var to_range = $("#to_range").val();
+      window.location.href = "genReportsPdf.php?from_range=" + from_range + "&to_range=" + to_range;
     });
   </script>
 </body>
